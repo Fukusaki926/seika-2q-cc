a65a1ddc045001ce461451816f3660a5d1e8774f
あなたの名前 <f221t155@st.kyoto-seika.ac.jp>
2023-08-09 23:35:22 +0900

    変更した

--- src/app.js
+++ src/app.js
@@ -15,3 +15,16 @@ for (let i = 0; i < 225; i++) {
 
   fragment.appendChild(el_item);
 }
+
+// アニメーションの対象となる要素を取得
+const element = document.getElementById('animated-element');
+
+// Anime.jsを使ってアニメーションを定義
+anime({
+  targets: element,
+  translateX: '500px', // 水平方向に500ピクセル移動
+  rotate: '1turn',     // 1回転する
+  backgroundColor: '#FF0000', // 背景色を赤に変更
+  duration: 2000,      // アニメーションの時間（ミリ秒）
+  easing: 'easeInOutQuad', // イージング関数（アニメーションの速さの変化を指定）
+});
